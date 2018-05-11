77f09e18621ce0db997e8e9c7d283f6e2e8c4154
zhihesi <2302521258@qq.com>
2018-05-11 21:41:48 +0800

    initial

---
+++ src/main/java/com/example/basequartz/BasequartzApplication.java
@@ -0,0 +1,12 @@
+package com.example.basequartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasequartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasequartzApplication.class, args);
+	}
+}
